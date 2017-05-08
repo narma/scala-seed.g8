242648edef7726867b45371fe1a38a25a35ef8e7
Sergey Rublev <sergey@foxcommerce.com>
2017-05-08 23:18:58 +0300

    Slighty modifications

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -1,5 +1,21 @@
 import sbt._
 
+object Versions {
+	val akka      = "2.5.7"
+	val cats      = "1.0.0-RC1"
+
+}
+
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
+
+  val akka = Seq(
+    "com.typesafe.akka" %% "akka-actor"     % Versions.akka,
+    "com.typesafe.akka" %% "akka-agent"     % Versions.akka,
+    "com.typesafe.akka" %% "akka-stream"    % Versions.akka
+  )
+
+  val cats = Seq(
+    "org.typelevel"         %% "cats"       % Versions.cats
+  )
+
 }

4fa93e82e005f6c6b41afbb944c0db9633169ad4
Sergey Rublev <s.rublev@elama.ru>
2018-06-14 14:37:30 +0300

    Fixes

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -1,15 +1,16 @@
 import Dependencies._
 
-lazy val root = (project in file(".")).
-  settings(
-    inThisBuild(List(
+lazy val root = (project in file(".")).settings(
+  inThisBuild(
+    List(
       organization := "com.example",
       scalaVersion := "2.12.6",
-      version      := "0.1.0-SNAPSHOT"
+      version := "0.1.0-SNAPSHOT"
     )),
-    name := "$name$",
-    libraryDependencies ++= {
-      import Dependencies._
-      core ++ logging ++ db ++ test
-    }
-  )
+  name := "$name$",
+  libraryDependencies ++= {
+    import Dependencies._
+    core ++ logging ++ db ++ tests
+  },
+  scalacOptions += "-Ypartial-unification"
+)
--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -1,10 +1,10 @@
 import sbt._
 
 object Versions {
-	val cats      = "1.1.0"
-  val Slf4j     = "1.7.19"
-  val Logback   = "1.2.3"
-  val doobie    = "0.5.3"
+  val cats = "1.1.0"
+  val Slf4j = "1.7.19"
+  val Logback = "1.2.3"
+  val doobie = "0.5.3"
   val flyway = "4.2.0"
   val circe = "0.10.0-M1"
 }
@@ -12,7 +12,7 @@ object Versions {
 object Dependencies {
 
   val logging = Seq(
-    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
+    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0",
     "org.slf4j" % "slf4j-api" % Versions.Slf4j,
     "ch.qos.logback" % "logback-classic" % Versions.Logback,
   )
@@ -25,23 +25,22 @@ object Dependencies {
   )
 
   val core = Seq(
-    "org.typelevel"         %% "cats"       % Versions.cats
-    "org.typelevel"         %% "cats-free"  % Versions.cats
+    "org.typelevel" %% "cats-core" % Versions.cats,
+    "org.typelevel" %% "cats-free" % Versions.cats
   ) ++ circe
 
   val doobie = Seq(
-     "org.tpolecat" %% "doobie-core"      % Versions.doobie,
-
-  // And add any of these as needed
-  // "org.tpolecat" %% "doobie-h2"        % Versions.doobie, // H2 driver 1.4.197 + type mappings.
-  "org.tpolecat" %% "doobie-hikari"    % Versions.doobie, // HikariCP transactor.
-  "org.tpolecat" %% "doobie-postgres"  % Versions.doobie, // Postgres driver 42.2.2 + type mappings.
-  // "org.tpolecat" %% "doobie-specs2"    % Versions.doobie, // Specs2 support for typechecking statements.
-  "org.tpolecat" %% "doobie-scalatest" % Versions.doobie  // ScalaTest support for typechecking statements.
+    "org.tpolecat" %% "doobie-core" % Versions.doobie,
+    // And add any of these as needed
+    // "org.tpolecat" %% "doobie-h2"        % Versions.doobie, // H2 driver 1.4.197 + type mappings.
+    "org.tpolecat" %% "doobie-hikari" % Versions.doobie, // HikariCP transactor.
+    "org.tpolecat" %% "doobie-postgres" % Versions.doobie, // Postgres driver 42.2.2 + type mappings.
+    // "org.tpolecat" %% "doobie-specs2"    % Versions.doobie, // Specs2 support for typechecking statements.
+    "org.tpolecat" %% "doobie-scalatest" % Versions.doobie // ScalaTest support for typechecking statements.
   )
 
   val db = Seq(
-     "org.flywaydb" % "flyway-core" % Version.flyway,
+    "org.flywaydb" % "flyway-core" % Versions.flyway,
   ) ++ doobie
 
   lazy val tests = Seq(
